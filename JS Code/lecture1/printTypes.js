3555f8b900b9382d03724839bdb72e88cc132bf7
shubham nikam <shubhamarunnikam2003@gmail.com>
2023-10-08 12:10:24 +0530

    created new repo of web dev

---
+++ JS Code/lecture1/printTypes.js
@@ -0,0 +1,64 @@
+// alert("hello shubham");
+// document.getElementById("a").innerHTML="hi i am shubham nikam from aurangabad";
+// document.write("Hello");
+// console.log("hello shubham");
+
+
+// declare variable(first method)
+
+//var a;
+//a=10;
+// alert(a);
+
+//or (second method of declartion)
+
+//let b;
+//b=15;
+//console.log(b);
+
+// let c= 6<7;
+
+// console.log(c);
+
+// let x= "shubham";
+// alert(`Hello ${x}`);
+
+
+//Condition
+
+// let age;
+// age=17;
+
+// if(age>=18)
+// {
+//     alert("you can voat");
+// }
+// else
+// {
+//     alert("false");
+// }
+
+// let a=0;
+// while(a<10)
+// {
+//     document.write("shubham");
+//     document.write('<br>')
+//     a ++
+// }
+
+
+// print sum of even number between 1 to 100(using while loops).
+
+
+// let counter = 1;
+// let sum = 0;
+// while(counter<=100)
+// {
+//     if(counter % 2 == 0)
+//     {
+//         sum = sum + counter;
+//     }
+//     counter++;
+// }
+// document.write(sum);
+
---
+++ JS Code/lecture2/app.js
@@ -0,0 +1,64 @@
+// Take input from user
+
+// alert("welcome to my website...!");
+// let nam = prompt('enter your name :-','xyz');
+// if(nam != null)
+// {
+//     document.write(`your name is ${nam}`);
+// }
+
+
+// Type conversion
+
+// let type = 5;
+// console.log(typeof type);
+
+// let newType = String(type);
+// console.log(typeof newType);
+
+// let str = "hi i am shubham nikam from aurangabad.i am learing js.";
+// console.log(str[5]);
+
+// let book = ["math","science","bio","computer science"];
+// book.pop();
+// console.log(book);
+
+// convert sentance to word
+
+// let text = "hi i am shubham from pune, i am studay computer engineering";
+// let newText = text.split('s');
+// console.log(newText);
+
+// multiple dimensionnal array
+
+// let bookWithpages = 
+// [
+//     ["math","300"],
+//     ["physics","400"],
+//     ["bio","350"]
+// ];
+
+// let fetch = bookWithpages[1][0];
+// console.log(fetch);
+
+// let table = prompt("enter the value of n");       //wrong
+// function my() {                                   // wrong
+// for(i=table; i<=table*10; i=i+table)               //wrong
+// {                                    
+//     document.write(table);                         //wrong
+//     document.write("<br>")                         //wrong
+// }
+// }
+
+// my();                                             //wrong
+
+
+let person = {
+    firstName: "Shubham",
+    lastName: "Nikam",
+    mobile : "9665409918"
+};
+console.log(person);
+
+let na = [];
+console.log(na);
